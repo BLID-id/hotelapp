8f9c6b05dc7ed29eb16267585770e31bc8aada03
BASTIEN <bastien.grenot@viacesi.fr>
2025-01-17 03:56:29 +0100

    commit sql files with tables

--- HotelAPI/HotelAPI/obj/Debug/net9.0/HotelAPI.AssemblyInfo.cs
+++ HotelAPI/HotelAPI/obj/Debug/net9.0/HotelAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c51127556e54f9b03be21a8f5fac020f87e25fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7258aad53d83e02493cff8b82b5067f85f09732")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
